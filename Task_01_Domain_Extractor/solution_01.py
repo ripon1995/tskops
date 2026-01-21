bbaa7742d74ce9fc6bdfaa108b07c479c633df6c
MD AL AMIN RIPON <rootless@MDs-MacBook-Pro.local>
2026-01-22 01:41:25 +0600

    python tasks initialized

---
+++ Task_01_Domain_Extractor/solution_01.py
@@ -0,0 +1,8 @@
+urls = [
+    "https://api.github.com/v3",
+    "https://scholar.google.com/",
+    "https://www.jstor.org/",
+    "https://www.sciencedirect.com/",
+    "https://www.researchgate.net/",
+    "https://www.researchgate.net/asdfadddaasdfasdfasdkf;alskdfjaoief;laskdfn;aeskijf;alksedfn;aleifalfnda;kfn;iefa;lekfn;aseifja;eifn;alskdfna;eif;aeifkn;askdfna;iefa;e",
+]
