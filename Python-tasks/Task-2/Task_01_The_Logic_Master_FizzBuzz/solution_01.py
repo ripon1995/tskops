a6f237688d5dea6d3b49e41f2a5821b00dd1907e
rootless <rootless@MDs-MacBook-Pro.local>
2026-01-28 23:31:39 +0600

    Task-2: solution-2

---
+++ Python-tasks/Task-2/Task_01_The_Logic_Master_FizzBuzz/solution_01.py
@@ -0,0 +1,13 @@
+def fizzbuzz_checker(limit):
+    for i in range(1, limit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizzbuzz_checker(15)
